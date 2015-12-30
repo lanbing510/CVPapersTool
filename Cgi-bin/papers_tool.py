26d16861f2210a0171ce6b2db31aa5c8cff134c7
lanbing510 <xiaosonglan@gmail.com>
2015-12-30 21:01:09 +0800

    Update

--- Cgi-bin/papers_tool.py
+++ Cgi-bin/papers_tool.py
@@ -10,13 +10,16 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 
 
-def readBuf(fsrc, length=1024):
+def readBuf(fsrc, length=16*1024):
     """解决一次性读取时出现的 self._sock.recv(left) error:[Errno 10054],分批读取并最后返回数据"""
+    result=''
     while 1:
         buf = fsrc.read(length)
         if not buf:
             break
-        return buf
+        else:
+            result+=buf
+    return result
 
 
 def loadUrl(url="http://www.cvpapers.com/cvpr2013.html"):
@@ -26,7 +29,6 @@ def loadUrl(url="http://www.cvpapers.com/cvpr2013.html"):
     request=urllib2.Request(full_url,headers=headers)
     response=urllib2.urlopen(request)
     response=readBuf(response)
-
     soup=BeautifulSoup(response)
     pool=soup.findAll('dt')
     despool={}
@@ -47,7 +49,6 @@ def loadUrl(url="http://www.cvpapers.com/cvpr2013.html"):
 
 def simplifyPapers(despool,keywords):
     keywords=keywords.split()
-
     #开始查找
     keydicpool={} #存放含有关键字的条目和相应含有的个数(供相关性排序)
     for i in range(len(despool)):
@@ -59,7 +60,6 @@ def simplifyPapers(despool,keywords):
             if len(includekey)!=0:
                 dl={i:len(includekey)}
                 keydicpool.update(dl)
-        
     #keylistpool=sorted(keydicpool.items(),key=lambda d:d[0],reverse=True)  
     keylistpool=sorted(keydicpool.items(),key=lambda d:d[1],reverse=True)
     return keylistpool
